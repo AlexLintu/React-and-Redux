6c7df4bc0206514c1946d51381283d10b65af075
Alex <alex@lumen5.com>
2019-08-17 21:46:01 -0700

    Add all of the counter logic

--- 10-counter/src/index.js
+++ 10-counter/src/index.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+// Redux Integration
+import { createStore } from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ACTION (function that returns and object)
+const increment = () => {
+  return {
+    type: "INCREMENT"
+  };
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const decrement = () => {
+  return {
+    type: "DECREMENT"
+  };
+};
+
+// REDUCER
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+};
+
+// STORE (Global State)
+let store = createStore(counter);
+
+// Display it in the console:
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATCH
+store.dispatch(increment());
+store.dispatch(decrement());
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
 serviceWorker.unregister();
