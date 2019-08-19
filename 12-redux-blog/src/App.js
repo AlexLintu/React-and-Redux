c09b663916b2c9e1888f9b97ed9c7f05d4dc82e6
Alex <alex@lumen5.com>
2019-08-19 10:41:11 -0700

    Add react portion

--- 12-redux-blog/src/App.js
+++ 12-redux-blog/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
-import "./App.css";
+
+import Posts from "./components/Posts";
+import PostForm from "./components/PostForm";
 
 function App() {
   return (
@@ -7,6 +9,9 @@ function App() {
       <header className="App-header">
         <h1>Redux Test</h1>
       </header>
+      <PostForm />
+      <hr />
+      <Posts />
     </div>
   );
 }
---
+++ 12-redux-blog/src/components/PostForm.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+
+class PostForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      body: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault(e);
+
+    const post = {
+      title: this.state.title,
+      body: this.state.body
+    };
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(post)
+    })
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }
+
+  render() {
+    return (
+      <div className="Form-body">
+        <h1>Add a Post</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label>Title: </label>
+            <br />
+            <input
+              type="text"
+              name="title"
+              onChange={this.handleChange}
+              value={this.state.title}
+            />
+          </div>
+          <div>
+            <label>Body: </label>
+            <br />
+            <textarea
+              name="body"
+              onChange={this.handleChange}
+              value={this.state.body}
+            />
+            <br />
+            <button type="submit">Add Post</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default PostForm;
